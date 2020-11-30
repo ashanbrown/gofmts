9194d814f49e78cbaf12de9fe3d373cd30d10c3c
Andrew S. Brown <andrew@launchdarkly.com>
2020-11-29 17:37:19 -0800

    Export a pre-commit hook

--- example/example.go
+++ example/example.go
@@ -4,26 +4,35 @@ package example
 
 //gofmts:sql
 const Sql = `
-SELECT * FROM mytable `
+		SELECT
+		  *
+		FROM
+		  mytable
+		`
 
 //gofmts:json
 const Json = `
-{"a":  1, "b":2, "c": [1, 2, 3]}`
+		{
+		  "a": 1,
+		  "b": 2,
+		  "c": [1, 2, 3]
+		}
+		`
 
 //gofmts:sort
-const Z = 1
 const A = 2
+const Z = 1
 
 const (
 	//gofmts:sort
 	// ignore this
-	Z1 = 1
 	A2 = 2
+	Z1 = 1
 )
 
 const (
 	//gofmts:sort
-	Z3 = 1
 	// move this
 	A3 = 2
+	Z3 = 1
 )
